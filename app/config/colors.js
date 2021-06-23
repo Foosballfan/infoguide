de9e18c0af85b2c96e4316fd80e7f636dc62e595
Tiril Fjell <tiril.fjell@gmail.com>
2021-06-23 09:35:12 +0200

    fiex nav

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,6 @@
 export default {
   primary: "#24AE80",
-  primaryLight: "rgba(36, 174, 128, 0.331)",
+  primaryLight: "rgba(36, 174, 128, 0.231)",
   secondary: "#7484F8",
   secondaryLight: "rgba(116, 132, 248, 0.213)",
   black: "#000",
